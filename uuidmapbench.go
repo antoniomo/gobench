77f0516d70c8241260679e4a1b80fc77cb50bde6
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2019-06-24 12:03:50 +0300

    Comparing GC times between strings and byte arrays

---
+++ uuidmapbench.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+
+	uuid "github.com/gofrs/uuid"
+)
+
+const (
+	numElements = 10000000
+)
+
+var foo = map[[16]byte]int{}
+
+func timeGC() {
+	t := time.Now()
+	runtime.GC()
+	fmt.Printf("gc took: %s\n", time.Since(t))
+}
+
+func main() {
+	for i := 0; i < numElements; i++ {
+		u := uuid.Must(uuid.NewV4())
+		foo[u] = i
+	}
+
+	for {
+		timeGC()
+		time.Sleep(1 * time.Second)
+	}
+}
